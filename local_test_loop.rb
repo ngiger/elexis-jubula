325e6f835a9262cb828c2af1a490cb4be445b7a7
Niklaus Giger <niklaus.giger@member.fsf.org>
2015-11-28 19:47:14 +0100

    Used nightly build. Longer time & better debug in local_test_loopt.rb

--- local_test_loop.rb
+++ local_test_loop.rb
@@ -3,9 +3,10 @@ require 'fileutils'
 require 'timeout'
 @nr_okay = 0
 @nr_failed = 0
+MAX_WAIT = 120
 
-def report
-  msg = "#{Time.now} #{@nr_okay + @nr_failed}: Had #{@nr_okay} sucesses and #{@nr_failed} failures"
+def report(msg = "#{@nr_okay + @nr_failed}: Had #{@nr_okay} sucesses and #{@nr_failed} failures")
+  msg = "#{Time.now}: #{msg}"
   puts msg
   File.open('local_test_loop.log', 'a+') {|f| f.puts msg}
 end
@@ -15,14 +16,15 @@ while true
   system('/opt/jubula_8.2.0.021/server/autagent -stop -p 6333')
   system('/opt/jubula_8.2.0.021/server/autagent -l -p 6333 &')
   report
-  break if (@nr_okay + @nr_failed) > 50
+  break if (@nr_okay + @nr_failed) > 100
   begin
-    timeout(60) do
-    result = system('mvn integration-test -Dtest=ch.ngiger.jubula.testsuites.Screenshot --offline')
+    timeout(MAX_WAIT) do
+    result = system('mvn clean integration-test -Dtest=ch.ngiger.jubula.testsuites.Screenshot --offline')
     result ? @nr_okay+=1 : @nr_failed += 1
   end
   rescue Timeout::Error
-    puts "#{Time.now}: Got timeout"
+    report "Got timeout after #{MAX_WAIT} seconds"
     @nr_failed += 1
+    FileUtils.cp('results/AUT_run.log', 'results/AUT_run.failed_' + @nr_failed.to_s, :verbose =>true)
   end
 end
