920d8cde2762638444dbbf0efa3eabf5949a22d5
Niklaus Giger <niklaus.giger@member.fsf.org>
2015-12-22 20:17:21 +0100

    Fixed VisitAllViews

--- jubula-tests/src/ch/ngiger/jubula/helpers/Views.java
+++ jubula-tests/src/ch/ngiger/jubula/helpers/Views.java
@@ -30,39 +30,30 @@ import org.junit.Test;
 
 import ch.ngiger.jubula.Messages;
 import ch.ngiger.jubula.elexiscore.OM;
+
 /** @author BREDEX GmbH */
 public class Views {
-	/** the logger */
-	// When using a logger the output is not shown in the maven output
-	// Don't know where it disappears
-	// private static Logger log = LoggerFactory.getLogger(VisitAllViews.class);
 
 	static void openViewByName(String name){
 		Common.openMenu("Fenster.*/Ansicht.*/Other.*");
 		Common.waitForWindow("Show View", Constants.ONE_SECOND);
 
-		org.eclipse.jubula.toolkit.concrete.components.TextInputComponent viewTxt = ConcreteComponents.createTextInputComponent(OM.ShowView_SelView_cti);
+		@SuppressWarnings("unchecked")
+		org.eclipse.jubula.toolkit.concrete.components.TextInputComponent viewTxt =
+			ConcreteComponents.createTextInputComponent(OM.ShowView_SelView_cti);
 		viewTxt.replaceText(name);
-//		Common.synchronizedTextReplace(OM.ShowView_SelView_cti, name);//$NON-NLS-1$
-
 		@SuppressWarnings("unchecked")
 		TableComponent tableComp =
 			ConcreteComponents.createTableComponent(OM.ShowView_ViewTree_grc);//$NON-NLS-1$
-		TreeTable tbl2 = SwtComponents.createTreeTable(OM.ShowView_ViewTree_grc) ;//$NON-NLS-1$
+		@SuppressWarnings("unchecked")
+		TreeTable tbl2 = SwtComponents.createTreeTable(OM.ShowView_ViewTree_grc);//$NON-NLS-1$
 		tbl2.waitForComponent(Constants.ONE_SECOND, 10);
 		AUT_run.m_aut.execute(tableComp.click(new Integer(1), InteractionMode.primary), null);
-		// Ub_tre_selectNode_byTextpath
-		AUT_run.m_aut.execute(tbl2.selectNodeByTextpath(SearchType.absolute,new Integer(0), name, Operator.matches, 1, InteractionMode.primary, BinaryChoice.no), null);
+		AUT_run.m_aut.execute(tbl2.selectNodeByTextpath(SearchType.absolute, new Integer(0), name,
+			Operator.matches, 1, InteractionMode.primary, BinaryChoice.no), null);
 
 		Common.clickComponent(OM.ShowView_OkButton_grc);
-
-		// Wait a little bit (workaround for Codes/Codes
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		Common.sleep1second();
 		Common.waitForElexisMainWindow(Constants.ONE_SECOND);
 	}
 
@@ -70,7 +61,7 @@ public class Views {
 	@Test
 	@SuppressWarnings("unchecked")
 	public void visit_all_views(AUT_run runner) throws Exception{
-		int mayor = 0, minor = 0, nr_views = 0;
+		int major = 0, minor = 0, nr_views = 0;
 		String new_pos = "first_time", new_pos2 = ""; //$NON-NLS-1$ //$NON-NLS-2$
 		ComponentIdentifier<Tree> tree = OM.ShowView_ViewTree_grc; //$NON-NLS-1$
 		Assert.assertNotNull("ShowView_ViewTree_grc may not be null", tree);
@@ -79,69 +70,81 @@ public class Views {
 		try {
 			String window_title = Messages.getString("VisitAllViews.4"); //$NON-NLS-1$
 			while (true) {
-				mayor++;
-				AUT_run.dbg_msg("Visiting view " + nr_views + " in row " + mayor + " open" + Messages.getString("VisitAllViews.7")); //$NON-NLS-1$ //$NON-NLS-2$
-				minor = 0;
-				while (true) {
-					Common.openMenu(Messages.getString("VisitAllViews.7")); //$NON-NLS-1$
-					Common.waitForWindow(window_title);
-					AUT_run.dbg_msg("window_title "+window_title); //$NON-NLS-1$
-					minor++;
-					new_pos = Integer.toString(mayor) + "/" + Integer.toString(minor); //$NON-NLS-1$
-					new_pos2 = Integer.toString(mayor) + "_" + Integer.toString(minor); //$NON-NLS-1$
-					AUT_run.takeScreenshotActiveWindow("iterate/pos_" + new_pos2 + ".png"); //$NON-NLS-1$ //$NON-NLS-2$
-					AUT_run.dbg_msg("Visiting view " + nr_views + " new_pos " + new_pos); //$NON-NLS-1$ //$NON-NLS-2$
-					try {
-						//						m_aut.execute(treeComp.checkEnablementOfContextMenuEntryByIndexpath(new_pos,
-						//true, InteractionMode.primary), null);
-						AUT_run.m_aut.execute(treeComp.expandNodeByIndexpath(SearchType.absolute,
-							new Integer(0), new_pos), null);
-						AUT_run.m_aut.execute(treeComp.selectNodeByIndexpath(SearchType.absolute,
-							new Integer(0), new_pos, new Integer(1), InteractionMode.primary,
-							ValueSets.BinaryChoice.no), null);
-						Common.clickComponent(OM.ShowView_OkButton_grc); //$NON-NLS-1$
-						Common.waitForElexisMainWindow();
-						Common.sleep1second(); // give view time to stabilize, eg. load a web page/patient
-						Common.maximixeView();
-						Common.sleep1second();
-						AUT_run.takeScreenshotActiveWindow("view_" + new_pos2 + ".png"); //$NON-NLS-1$ //$NON-NLS-2$
-						nr_views++;
-					} catch (ComponentNotFoundException e) {
-						AUT_run.dbg_msg("ComponentNotFoundException with " + new_pos); //$NON-NLS-1$
-						if (nr_views > 20) {
-							AUT_run.takeScreenshotActiveWindow(
-								"list_too_long_after_" + nr_views + ".png"); //$NON-NLS-1$ //$NON-NLS-2$
-							Assert.fail("We cannot handle very long lists of vies"); //$NON-NLS-1$
-							// breaks when nr_views == 52
+				try {
+					major++;
+					AUT_run.dbg_msg(
+						String.format("Visiting view %d in row %d open %s", nr_views, major, //$NON-NLS-1$
+							Messages.getString("VisitAllViews.7")));//$NON-NLS-1$
+					minor = 0;
+					while (true) {
+						minor++;
+						Common.openMenu(Messages.getString("VisitAllViews.7")); //$NON-NLS-1$
+						Common.waitForWindow(window_title);
+						AUT_run.dbg_msg("window_title " + window_title); //$NON-NLS-1$
+						new_pos = Integer.toString(major) + "/" + Integer.toString(minor); //$NON-NLS-1$
+						new_pos2 = Integer.toString(major) + "_" + Integer.toString(minor); //$NON-NLS-1$
+						try {
+							//						m_aut.execute(treeComp.checkEnablementOfContextMenuEntryByIndexpath(new_pos,
+							//true, InteractionMode.primary), null);
+							AUT_run.m_aut.execute(treeComp.expandNodeByIndexpath(
+								SearchType.absolute, new Integer(0), new_pos), null);
+							AUT_run.m_aut.execute(treeComp.selectNodeByIndexpath(
+								SearchType.absolute, new Integer(0), new_pos, new Integer(1),
+								InteractionMode.primary, ValueSets.BinaryChoice.no), null);
+							Common.clickComponent(OM.ShowView_OkButton_grc); //$NON-NLS-1$
+							Common.waitForElexisMainWindow();
+							Common.sleep1second(); // give view time to stabilize, eg. load a web page/patient
+							Common.maximixeView();
+							Common.sleep1second();
+							AUT_run.takeScreenshotActiveWindow("view_" + new_pos2 + ".png"); //$NON-NLS-1$ //$NON-NLS-2$
+							nr_views++;
+						} catch (ComponentNotFoundException e) {
+							if (nr_views > 20) {
+								AUT_run.takeScreenshotActiveWindow(
+									"list_too_long_after_" + nr_views + ".png"); //$NON-NLS-1$ //$NON-NLS-2$
+								Assert.fail("We cannot handle very long lists of vies"); //$NON-NLS-1$
+								// breaks when nr_views == 52
+							}
+							break;
+						} catch (ActionException e) {
+							if (minor == 1) {
+								AUT_run.dbg_msg(String.format(
+									"ActionException minor with %s -> No more entries found for %d %d. Found  %d", //$NON-NLS-1$
+									new_pos, major, minor, nr_views));
+								AUT_run.takeScreenshotActiveWindow("minor 1.png"); //$NON-NLS-1$
+								return; // No more entries found for mayor
+							}
+							minor = 0;
+							AUT_run.takeScreenshotActiveWindow(new_pos + "_minor 0.png"); //$NON-NLS-1$
+							Common.clickComponent(OM.ShowView_OkButton_grc); //$NON-NLS-1$
+							Common.waitForWindowClose(window_title);
+							break;
 						}
-						break;
-					} catch (ActionException e) {
-						AUT_run.dbg_msg("ActionException minor with " + new_pos); //$NON-NLS-1$
-						if (minor == 1) {
-							AUT_run.dbg_msg("ActionException minor with " + new_pos //$NON-NLS-1$
-								+ " -> No more entries found for mayor" + ". Found " + nr_views //$NON-NLS-1$ //$NON-NLS-2$
-								+ " views!"); //$NON-NLS-1$
-							return; // No more entries found for mayor
-						}
-						minor = 0;
-						Common.clickComponent(OM.ShowView_OkButton_grc); //$NON-NLS-1$
-						Common.waitForWindowClose(window_title);
-						break;
+					}
+				} catch (ActionException e) {
+					if (minor == 1) {
+						AUT_run.dbg_msg(String.format(
+							"ActionException minor with %s -> No more entries found for %d %d. Found  %d views.", //$NON-NLS-1$
+							new_pos, major, minor, nr_views));
+						return; // No more entries found for mayor
 					}
 				}
 			}
 		} catch (ActionException e) {
-			AUT_run.dbg_msg("ActionException major with " + nr_views + " pos " + new_pos); //$NON-NLS-1$ //$NON-NLS-2$
+			AUT_run.dbg_msg(String.format("ActionException major with %d pos %s. Having %d views", //$NON-NLS-1$
+				nr_views, new_pos, nr_views));
 			if (nr_views == 0)
 				e.printStackTrace();
-			if (nr_views <= 10) {
-				Assert.fail("We should have more than 10 views to visit!"); //$NON-NLS-1$
-			}
+			e.printStackTrace();
 
 		} catch (CheckFailedException | AssertionError e) {
+			AUT_run.dbg_msg("visit_all_views: error was " + e.getMessage());
+			e.printStackTrace();
+			AUT_run.takeScreenshotActiveWindow("CheckFailedException.png"); //$NON-NLS-1$
 			e.printStackTrace();
 		} finally {
-			Assert.assertTrue(true);
+			AUT_run.dbg_msg("visit_all_views: done. Found views: " + nr_views);
+			Assert.assertTrue("We should have more than 10 views to visit!", nr_views >= 10 ); //$NON-NLS-1$
 		}
 	}
 }
