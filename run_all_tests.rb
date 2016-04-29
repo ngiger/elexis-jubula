6cb4516f06049f80163744866a7264c546ddba86
Niklaus Giger <niklaus.giger@member.fsf.org>
2016-04-29 09:49:33 +0200

    Document test environmnet. Added run_all_tests.rb

--- run_all_tests.rb
+++ run_all_tests.rb
@@ -6,7 +6,6 @@ tests = ['Screenshot',
 'SuiteVisitAllViews',
 'SuiteVisitAllPreferencePages',]
 
-# tests = ['Smoketest',]
 tests.each do |test|
   puts test
   system("bundle exec rake jubula_docker[#{test}] 2>&1 | tee all_#{test}.log")
--- scripts/jubularunner.rb
+++ scripts/jubularunner.rb
@@ -128,10 +128,12 @@ class JubulaRunner
   def prepare_docker
     @docker.stop_docker
     at_exit { @docker.stop_docker }
-    FileUtils.rm_rf(@docker.container_home, verbose: true)
-    fail "Must be possible to remove container_home #{@docker.container_home}" if File.exist?(@docker.container_home)
-    ['.git', 'pom.xml', 'jubula-target', 'jubula-tests', 'org.eclipse.jubula.product.autagent.start'].each do |item|
-      FileUtils.cp_r(File.join(RootDir, item), @docker.container_home, verbose: true, preserve: true)
+    if File.exists?(@docker.container_home)
+      FileUtils.mv(@docker.container_home, "/tmp/#{Time.now.strftime('%Y%m%d%H%M%s')}", verbose: true)
+      fail "Must be possible to remove container_home #{@docker.container_home}" if File.exist?(@docker.container_home)
+      ['.git', 'pom.xml', 'jubula-target', 'jubula-tests', 'org.eclipse.jubula.product.autagent.start'].each do |item|
+        FileUtils.cp_r(File.join(RootDir, item), @docker.container_home, verbose: true, preserve: true)
+      end
     end
     FileUtils.makedirs(@docker.container_home)
     FileUtils.cp_r(WorkDir, File.join(@docker.container_home, 'work'), verbose: true, preserve: true)
