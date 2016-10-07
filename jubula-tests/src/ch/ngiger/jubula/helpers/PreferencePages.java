6c295a27c5235948268b182f46d0bec4d9296599
Niklaus Giger <niklaus.giger@member.fsf.org>
2016-10-07 17:51:49 +0200

    Create named preferences.png

--- jubula-tests/src/ch/ngiger/jubula/helpers/PreferencePages.java
+++ jubula-tests/src/ch/ngiger/jubula/helpers/PreferencePages.java
@@ -11,17 +11,18 @@
 package ch.ngiger.jubula.helpers;
 
 import org.eclipse.jubula.client.AUT;
+import org.eclipse.jubula.client.Result;
 import org.eclipse.jubula.client.exceptions.ActionException;
 import org.eclipse.jubula.client.exceptions.CheckFailedException;
 import org.eclipse.jubula.client.exceptions.CommunicationException;
 import org.eclipse.jubula.client.exceptions.ComponentNotFoundException;
 import org.eclipse.jubula.client.exceptions.ConfigurationException;
 import org.eclipse.jubula.toolkit.concrete.components.Application;
-import org.eclipse.jubula.toolkit.concrete.components.ButtonComponent;
 import org.eclipse.jubula.toolkit.concrete.components.TreeComponent;
 import org.eclipse.jubula.toolkit.enums.ValueSets;
 import org.eclipse.jubula.toolkit.enums.ValueSets.InteractionMode;
 import org.eclipse.jubula.toolkit.enums.ValueSets.SearchType;
+import org.eclipse.jubula.toolkit.swt.SwtComponents;
 import org.eclipse.jubula.toolkit.swt.components.Button;
 import org.eclipse.jubula.toolkit.swt.components.Tree;
 import org.eclipse.jubula.tools.ComponentIdentifier;
@@ -44,35 +45,49 @@ public class PreferencePages extends Common {
 
 	boolean gotoPreferencPage(String position){
 		String window_title = Messages.getString("VisitAllPreferencePages.4"); //$NON-NLS-1$
-		// AUT_run.dbg_msg("gotoPreferencPage: " + position);
-		waitForWindowClose(window_title);
-		waitForElexisMainWindow(Constants.ONE_SECOND);
+		Assert.assertTrue("gotoPreferencPage: waitForWindowClose " + window_title, waitForWindowClose(window_title));
+		Assert.assertTrue("gotoPreferencPage: Elexis-MainWindow", waitForElexisMainWindow(Constants.ONE_SECOND));
 		openMenu(Messages.getString("VisitAllPreferencePages.7")); //$NON-NLS-1$
 		try {
+			@SuppressWarnings("unchecked")
+			ComponentIdentifier<Tree> tree = OM.Preferences_ItemChoice_tre; //$NON-NLS-1$
+			TreeComponent treeComp = org.eclipse.jubula.toolkit.concrete.ConcreteComponents.createTreeComponent(tree);
+			clickComponent(tree); // This click is only necessary after visiting "Security Storage"! Took me 3h to find it
 			AUT_run.m_aut.execute(
 				treeComp.selectNodeByIndexpath(SearchType.absolute, new Integer(0), position,
 					new Integer(1), InteractionMode.primary, ValueSets.BinaryChoice.no),
 				null);
-		} catch (ActionException e) {
-			Utils.dbg_msg("gotoPreferencPage " + position + " msg: " + e.getMessage()); //$NON-NLS-1$
+		} catch (ActionException | ComponentNotFoundException e) {
+			// Utils.dbg_msg("gotoPreferencPage e: " + position + " msg: " + e.getMessage()); //$NON-NLS-1$
 			return false;
 		}
-		// AUT_run.dbg_msg("gotoPreferencPage " + position + " done"); //$NON-NLS-1$
 		return true;
 	}
 
-	TreeComponent treeComp = null;
-	ButtonComponent ok_btn_comp = null;
+	private String create_named_preference_screenshot(String prefix){
+		try {
+			@SuppressWarnings("unchecked")
+			org.eclipse.jubula.toolkit.concrete.components.TextComponent txt_comp = SwtComponents.createTextComponent(OM.Preference_title_txt);
+			Result<Object> res = m_aut.execute(txt_comp.readValue(), null);
+			String name = res.getReturnValue();
+			String pref_name = 	"preferences/" +  prefix + "_"+ name +".png";//$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+			AUT_run.takeScreenshotActiveWindow(m_aut, m_app, pref_name);
+			return name;
+		} catch (ActionException | ComponentNotFoundException e) {
+			Utils.dbg_msg("create_named_preference_screenshot " + prefix + " unable to get text: " + e.getMessage()); //$NON-NLS-1$
+			AUT_run.takeScreenshotActiveWindow(m_aut, m_app,
+				"preferences/page_" + prefix + ".png"); //$NON-NLS-1$ //$NON-NLS-2$
+			return prefix;
+		}
+	}
 
 	/** test visiting all preferencePages */
-	@SuppressWarnings({
-		"unchecked"
-	})
+	@SuppressWarnings("unchecked")
 	public void visit_all_preferencePages() throws Exception{
 		String new_pos = "first_time"; //$NON-NLS-1$ //$NON-NLS-2$
 		int nr_preferencePages = 0;
+		@SuppressWarnings("unused")
 		ComponentIdentifier<Tree> tree = OM.Preferences_ItemChoice_tre; //$NON-NLS-1$
-		treeComp = org.eclipse.jubula.toolkit.concrete.ConcreteComponents.createTreeComponent(tree);
 		ComponentIdentifier<Button> ok_btn = OM.Preferences_OkButton_grc; //$NON-NLS-1$
 		ComponentIdentifier<Button> apply_btn = OM.Preferences_ApplyButton_grc; //$NON-NLS-1$
 		ComponentIdentifier<Button> cancel_btn = OM.Preferences_Cancel_btn; //$NON-NLS-1$
@@ -81,8 +96,6 @@ public class PreferencePages extends Common {
 		Assert.assertNotNull("Preferences_ApplyButton_grc may not be null", apply_btn);
 		Assert.assertNotNull("Preferences_Cancel_btn may not be null", cancel_btn);
 		Assert.assertNotNull("ResetPerspektive_OkButton_grc", reset_btn);
-		ok_btn_comp =
-			org.eclipse.jubula.toolkit.concrete.ConcreteComponents.createButtonComponent(ok_btn);
 		try {
 			int major = 1;
 			while (true) {
@@ -93,8 +106,7 @@ public class PreferencePages extends Common {
 					break;
 				}
 				nr_preferencePages++;
-				AUT_run.takeScreenshotActiveWindow(m_aut, m_app,
-					"preferences/page_" + new_pos + ".png"); //$NON-NLS-1$ //$NON-NLS-2$
+				create_named_preference_screenshot(new_pos);
 				clickComponent(ok_btn);
 				while (true) {
 					new_pos = Integer.toString(major) + "/" + Integer.toString(minor); //$NON-NLS-1$
@@ -103,17 +115,17 @@ public class PreferencePages extends Common {
 						break;
 					}
 					nr_preferencePages++;
-					AUT_run.takeScreenshotActiveWindow(m_aut, m_app,
-						"preferences/page_" + new_pos.replace("/", "_") + ".png"); //$NON-NLS-1$ //$NON-NLS-2$
+					create_named_preference_screenshot(new_pos.replace("/", "_"));
 					try {
 						clickComponent(ok_btn);
-					} catch (ActionException e) {
-						// happens with security!
+						// the ok_btn is not present in the Security/Secure Storage!!
+						// therefore we must catch also the ComponentNotFoundException
+					} catch (ActionException | ComponentNotFoundException e) {
 						Utils.dbg_msg("visit_all_preferencePages. must cancel. Got exception: "
 							+ e.getMessage());
 						AUT_run.takeScreenshotActiveWindow(m_aut, m_app,
 							"preferences/cancel_" + new_pos.replace("/", "_") + ".png"); //$NON-NLS-1$ //$NON-NLS-2$
-						clickComponent(cancel_btn);
+						clickComponent(ok_btn);
 					}
 					minor++;
 				}
@@ -122,14 +134,14 @@ public class PreferencePages extends Common {
 		} catch (ComponentNotFoundException | CommunicationException | ConfigurationException
 				| CheckFailedException | ActionException | AssertException e) {
 			Utils.dbg_msg("Got exception: " + e.getMessage());
-			AUT_run.takeScreenshotActiveWindow(m_aut, m_app, "preferences/exception.png"); //$NON-NLS-1$
+			AUT_run.takeScreenshotActiveWindow(m_aut, m_app, "preferences/" + new_pos.replace("/", "_") + "_exception.png"); //$NON-NLS-1$ //$NON-NLS-2$
 			e.printStackTrace(Utils.getWriter());
 
 		} finally {
 			Utils.dbg_msg(
 				"visit_all_preferencePages done. nr_preferencePages  is " + nr_preferencePages);
 		}
-		AUT_run.takeScreenshotActiveWindow(m_aut, m_app, "preferences/last_page.png"); //$NON-NLS-1$
+		AUT_run.takeScreenshotActiveWindow(m_aut, m_app, "preferences/last_page_" + new_pos.replace("/", "_") + ".png"); //$NON-NLS-1$ //$NON-NLS-2$
 		Assert.assertTrue("Must visit at least 10 preferences", nr_preferencePages > 10);
 	}
 }
