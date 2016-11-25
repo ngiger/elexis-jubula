57f727b042021cbcc8abfb06b4b2e6982f2123ec
Niklaus Giger <niklaus.giger@member.fsf.org>
2016-11-26 00:03:00 +0100

    Run Preferences as last Suite for Medelexis

--- jubula-tests/src/ch/ngiger/jubula/testsuites/Medelexis.java
+++ jubula-tests/src/ch/ngiger/jubula/testsuites/Medelexis.java
@@ -8,8 +8,8 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
    Smoketest.class,
    SuiteVisitAllPerspectives.class,
-   SuiteVisitAllPreferencePages.class,
-   SuiteVisitAllViews.class
+   SuiteVisitAllViews.class,
+   SuiteVisitAllPreferencePages.class
 })
 
 public class Medelexis {
